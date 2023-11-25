c80fa8230df5c98a38666e6e11dbad5ac7c64f0e
BEN TREWICK <B.TREWICK-2022@hull.ac.uk>
2023-11-25 13:05:21 +0000

    Intractable box thing

--- Assets/Scripts/Interactables.cs
+++ Assets/Scripts/Interactables.cs
@@ -8,9 +8,41 @@ public class Interactables : MonoBehaviour
     public bool isInRange;
     public KeyCode interactKey;
     public UnityEvent interactAction;
+    public GameObject outline;
 
     private void Start()
     {
         
+    }
+
+    private void Update()
+    {
+        if (isInRange)
+        {
+            if(Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("Robot is in range");
+            outline.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("Robot is not in range");
+            outline.SetActive(false);
+        }
     }
 }
