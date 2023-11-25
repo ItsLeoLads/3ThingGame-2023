c28bb361a5514a0ceb78ac9f8fa3306947253808
BEN TREWICK <B.TREWICK-2022@hull.ac.uk>
2023-11-25 12:26:57 +0000

    literally nothing as leo is a bozo

---
+++ Assets/Scripts/Interactables.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactables : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+
+    private void Start()
+    {
+        
+    }
+}
