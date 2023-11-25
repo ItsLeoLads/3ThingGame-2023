af5cac3004a37baf9d4e8cda74d5d9f46da34e43
772810 <772810@FEN-052-A-13>
2023-11-25 14:25:12 +0000

    Created MainMenu, just need title

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//I got this code from https://www.youtube.com/watch?v=DX7HyN7oJjE 
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadSceneAsync(1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
